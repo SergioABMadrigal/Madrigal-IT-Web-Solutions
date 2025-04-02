d286805c2f38731f1341ec8a083c0441d0867257
Sergio <sergio.a.b.madrigal@gmail.com>
2025-04-02 12:07:01 -0400

    Remove .gitignore file, add motion dependency, and implement FloatingShape component; update Contact and Solutions pages for improved UI

---
+++ frontend/src/Components/FloatingShape.jsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+import PropTypes from "prop-types";
+
+const FloatingShape = ({ color, size, top, left, delay }) => {
+	return (
+		<motion.div
+			className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl`}
+			style={{ top, left }}
+			animate={{
+				y: ["0%", "100%", "0%"],
+				x: ["0%", "100%", "0%"],
+				rotate: [0, 360],
+			}}
+			transition={{
+				duration: 20,
+				ease: "linear",
+				repeat: Infinity,
+				delay,
+			}}
+			aria-hidden='true'
+		/>
+	);
+};
+
+FloatingShape.propTypes = {
+	color: PropTypes.string.isRequired,
+	size: PropTypes.string.isRequired,
+	top: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+	left: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+	delay: PropTypes.number.isRequired,
+};
+
+export default FloatingShape;
--- frontend/src/Pages/Solutions.jsx
+++ frontend/src/Pages/Solutions.jsx
@@ -1,28 +1,33 @@
 
+import FloatingShape from '../Components/FloatingShape.jsx';
+
 const Solution = () => {
   return (
     <div className="flex flex-col items-center justify-center mx-10 gap-4 py-20">
+      <FloatingShape/>
+      <FloatingShape color="bg-[#3f4d55]" size="w-32 h-32" top="5%" left="20%" delay={0} />
+      <FloatingShape color="bg-[#1f1e26]" size="w-48 h-48" top="30%" left="50%" delay={2} />
+      <FloatingShape color="bg-[#3f4d55]" size="w-64 h-64" top="50%" left="80%" delay={4} />
       <div className="flex flex-col gap-4 w-1/2 text-center">
-        <h1 className="text-2xl font-bold">We offer a number of tiers to meet your website needs.</h1>
-        <p>Why Choose Us?</p>
+      <h1 className="text-2xl font-bold">We offer a number of tiers to meet your website needs.</h1>
+      <p>Why Choose Us?</p>
       </div>
-      <div className="flex flex-col gap-4 w-2/3">
-        
-        <ul className="list-disc list-inside text-left">
-          <li>Professional Websites bring in and hold customers.</li>
-          <li>We manage the website in a Virtual Private Network. The server is fully under our control.</li>
-          <li>Security updates and patches are included.</li>
-          <li>Search Engine Optimization is conducted INSIDE the code to ensure fast loading times and Search Engine compatibility</li>
-          <li>We use our own templates, with higher customization than DIY paid sites.</li>
-          <li>Real Domains used. We search, buy, and connect the domain name for you. The Domain name is YOURS to transfer if needed.</li>
-          <li>Our websites are optimized for mobile use at no extra cost.</li>
-          <li>We implement SSL certificates at no extra cost.</li>
-          <li>Continuous research from us means more affordable features to come.</li>
-        </ul>
-        <p>Our development and hosting structure reduces costs per website. Savings are passed to YOU.</p>
+      <div className="flex flex-col gap-4 w-2/3 items-center">
+      <ul className="list-disc list-inside text-left w-3/4">
+        <li>Professional Websites bring in and hold customers.</li>
+        <li>We manage the website in a Virtual Private Network. The server is fully under our control.</li>
+        <li>Security updates and patches are included.</li>
+        <li>Search Engine Optimization is conducted INSIDE the code to ensure fast loading times and Search Engine compatibility</li>
+        <li>We use our own templates, with higher customization than DIY paid sites.</li>
+        <li>Real Domains used. We search, buy, and connect the domain name for you. The Domain name is YOURS to transfer if needed.</li>
+        <li>Our websites are optimized for mobile use at no extra cost.</li>
+        <li>We implement SSL certificates at no extra cost.</li>
+        <li>Continuous research from us means more affordable features to come.</li>
+      </ul>
+      <p className="text-center">Our development and hosting structure reduces costs per website. Savings are passed to YOU.</p>
       </div>
     </div>
-  )
+    )
 }
 
 export default Solution
