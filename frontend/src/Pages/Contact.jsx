c214544fe1ca4afbf65fd0cf2fcf945dca05d2f2
Sergio <sergio.a.b.madrigal@gmail.com>
2025-04-02 12:07:01 -0400

    Remove .gitignore file, add motion dependency, and implement FloatingShape component; update Contact and Solutions pages for improved UI

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 
 const Contact = () => {
   return (
     <div className='flex flex-col items-center justify-center px-20 py-15'>
       <h1 className='text-3xl font-bold mb-4'>Contact Us</h1>
-      <p className='text-lg mb-2'>Email: smadrigal@madrigalit.dev</p>
-      <p className='text-lg mb-2'>Phone: 413-961-9257</p>
+      <p className='text-xl mb-2'>Email: smadrigal@madrigalit.dev</p>
+  
       
     </div>
   )
