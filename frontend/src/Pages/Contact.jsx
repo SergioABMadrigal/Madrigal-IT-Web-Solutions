119242ac7e4739d85f6697efa648d94496124ab7
Sergio <sergio.a.b.madrigal@gmail.com>
2025-04-06 13:11:47 -0400

    Add FloatingShape component and enhance responsiveness across pages

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -3,9 +3,10 @@
 const Contact = () => {
   return (
     <div className='flex flex-col items-center justify-center px-20 py-15'>
+
       <h1 className='text-3xl font-bold mb-4'>Contact Us</h1>
       <p className='text-xl mb-2'>Email: smadrigal@madrigalit.dev</p>
-  
+    
       
     </div>
   )
