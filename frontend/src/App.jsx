c8338a6f586e166e2332bef0ed5630a7a41bef4a
SergioABMadrigal <sergio.a.b.madrigal@gmail.com>
2025-04-07 11:07:13 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,12 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Solutions from './Pages/Solutions';
 import Error404 from './Pages/Error404';
-import FloatingShape from './Components/FloatingShape';
 
 function App() {
 
   return (
     <div className="flex flex-col min-h-screen">
-        <FloatingShape color='bg-blue-900' size='w-32 h-32' top='-5%' left='10%' delay={0} />
-        <FloatingShape color='bg-yellow-500' size='w-24 h-24' top='70%' left='80%' delay={5} />
-        <FloatingShape color='bg-yellow-400' size='w-16 h-16' top='40%' left='-10%' delay={2} />
-        <FloatingShape color='bg-blue-800' size='w-20 h-20' top='20%' left='50%' delay={3} />
-        <FloatingShape color='bg-yellow-600' size='w-28 h-28' top='80%' left='20%' delay={7} />
+        
         
       <Navbar />
 
