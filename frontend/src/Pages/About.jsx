edfb41d0263cef0316f6c4d348e220b34c6ac1dc
SergioABMadrigal <sergio.a.b.madrigal@gmail.com>
2025-05-14 20:08:08 -0400

    feat: enhance visibility of "Why Choose Us?" heading by adding font-bold class

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -16,23 +16,11 @@ const About = () => {
   const isVisible3 = useIsVisible(ref3);
 
   return (
-    <div className={`flex flex-col items-center ${window.innerWidth < 800 ? 'px-2 py-5' : 'px-5 py-10'} w-full`}>
-      <div className='w-full max-w-5xl'>
-      <h1 className='text-center text-6xl'>About Us</h1>
-      <div ref={ref1} className={`py-5 transition-opacity ease-in duration-700 ${isVisible1 ? "opacity-100" : "opacity-0"}`}>
-      <p className='text-2xl indent-4'>
-      Welcome to Madrigal IT Web Solutions, your trusted partner for website development services. We specialize in creating professional and user-friendly websites for small businesses and startups. Our goal is to help you establish a strong online presence and achieve your business objectives.
-      </p>
-      </div>
-      <div ref={ref2} className={`py-5 transition-opacity ease-in duration-700 ${isVisible2 ? "opacity-100" : "opacity-0"}`}>
-      <p className='text-2xl indent-4'>
-      We work closely with you to understand your unique needs and deliver customized solutions that align with your brand. Whether you need a simple informational site or a complex e-commerce platform, we have the expertise to bring your vision to life.
-      </p>
-      </div>
-      <div ref={ref3} className={`py-5 transition-opacity ease-in duration-700 ${isVisible3 ? "opacity-100" : "opacity-0"}`}>
-      <p className='text-2xl indent-4'>
-      At Madrigal IT Web Solutions, we are committed to delivering high-quality services and exceptional customer support. Let us help you take your business to the next level with a website that stands out.
-      </p>
+    <div className={`flex flex-col items-center justify-center gap-4 ${window.innerWidth < 800 ? 'px-2 py-4 w-full' : 'mx-10 py-20'}`}>
+      
+      <div className={`flex flex-col gap-4 ${window.innerWidth < 800 ? 'w-full' : 'w-1/2'} text-center`}>
+        <h1 className="text-4xl font-bold">We offer a number of tiers to meet your website needs.</h1>
+        <h2 className="text-2xl text-green-800 font-bold p-4 rounded-md" style={{ maxWidth: '400px', margin: '0 auto' }}>Why Choose Us?</h2>
       </div>
       </div>
     </div>
