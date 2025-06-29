a5538ef80449a0d3ef142930f44ce66165a04d0d
SergioABMadrigal <sergio.a.b.madrigal@gmail.com>
2025-06-29 18:10:00 -0400

    feat: update Blog link in Navbar to open in a new tab with appropriate attributes

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -47,7 +47,7 @@ return (
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="https://madrigalit.wordpress.com/" className="flex flex-col items-center gap-1">
+                        <NavLink to="https://madrigalit.wordpress.com/" className="flex flex-col items-center gap-1" target="_blank" rel="noopener noreferrer">
                             <p>Blog</p>
                             <hr className="w-2/4 border-none h-[1.5px] bg-amber-300 hidden" />
                         </NavLink>
@@ -81,7 +81,7 @@ return (
                               <NavLink onClick={()=>setVisible(false)} className={({ isActive }) => isActive ? 'py-2 pl-6 border text-white bg-[#3f4d55]' : 'py-2 pl-6 border'} to='/about' >ABOUT</NavLink>
                               <NavLink onClick={()=>setVisible(false)} className={({ isActive }) => isActive ? 'py-2 pl-6 border text-white bg-[#3f4d55]' : 'py-2 pl-6 border'} to='/contact' >CONTACT</NavLink>
                               <NavLink onClick={() => setVisible(false)} className={({ isActive }) => isActive ? 'py-2 pl-6 border text-white bg-[#3f4d55]' : 'py-2 pl-6 border'} to='/solutions'>SOLUTIONS</NavLink>
-                              <NavLink onClick={() => setVisible(false)} className={({ isActive }) => isActive ? 'py-2 pl-6 border text-white bg-[#3f4d55]' : 'py-2 pl-6 border'} to='/blog'>BLOG</NavLink>
+                              <NavLink onClick={() => setVisible(false)} to="https://madrigalit.wordpress.com/" className="py-2 pl-6 border" target="_blank" rel="noopener noreferrer">BLOG</NavLink>
                               <NavLink onClick={() => setVisible(false)} className={({ isActive }) => isActive ? 'py-2 pl-6 border text-white bg-[#3f4d55]' : 'py-2 pl-6 border'} to='https://github.com/SergioABMadrigal/Madrigal-IT-Web-Solutions/tree/new-branch-from-c663149'>GITHUB REPO</NavLink>
 
                               {/* <NavLink onClick={() => setVisible(false)} className={({ isActive }) => isActive ? 'py-2 pl-6 border text-white bg-[#3f4d55]' : 'py-2 pl-6 border'} to='/artdisplay'>ART DISPLAY</NavLink> */}
